585850ae1426c86abdbfd86876b5d74e1175d0f8
fortishield <161459699+FortiShield@users.noreply.github.com>
2024-11-13 19:06:18 +0600

    khulnasoft/release (#1)
    * khulnasoft/release
    
    * Updates most languages and tree-sitter to 0.23.

--- download_parse.sh
+++ download_parse.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
-VERSION="v0.0.16"
+VERSION="v0.0.17"
 rm -f parse.gz parse
-curl -Lo parse.gz "https://github.com/KhulnaSoft/khulnasoft-parse/releases/download/$VERSION/parse.gz"
+curl -Lo parse.gz "https://github.com/khulnasoft/khulnasoft-parse/releases/download/$VERSION/parse.gz"
 gzip -d parse.gz
 chmod +x parse
